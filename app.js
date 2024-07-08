8f24168c7f0a5fca35b5dd19f34e4f112e568d31
Valdimir Ferraz <valdimirferraz@hotmail.com>
2024-07-07 22:41:45 -0300

    alterado teste

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('h1', 'Acertou!!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
